5083117f28c6fa08a3a2e56bc5da68fe804c1ae3
Ajayi Davies <ajayidavies7@gmail.com>
2023-05-06 11:24:39 +0200

    splitlines and startwith methods

--- markdown2html.py
+++ markdown2html.py
@@ -4,25 +4,27 @@
 import sys
 
 if __name__ == "__main__":
-    n = 0
-    line = ""
-    start = 0
-    count = 0
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
     try:
         with open(sys.argv[1], "r") as f:
             contents = f.read()
+            lines = contents.splitlines()
         with open(sys.argv[2], "w") as f:
-            for i in range(len(contents)):
-                if contents[i] == "#":
-                    n += 1
-                if contents[i] == "\n" and contents[i + 1] != "":
-                    start = n + count
-                    f.write(f"<h{n}>{contents[start + 1 : i]}</h{n}>\n")
-                    count = i
-                    n = 0
+            for line in lines:
+                if line.startswith("# "):
+                    f.write("<h1>" + line[2:] + "</h1>\n")
+                elif line.startswith("## "):
+                    f.write("<h2>" + line[3:] + "</h2>\n")
+                elif line.startswith("### "):
+                    f.write("<h3>" + line[4:] + "</h3>\n")
+                elif line.startswith("#### "):
+                    f.write("<h4>" + line[5:] + "</h4>\n")
+                elif line.startswith("##### "):
+                    f.write("<h5>" + line[6:] + "</h5>\n")
+                elif line.startswith("###### "):
+                    f.write("<h6>" + line[7:] + "</h6>\n")
         sys.exit(0)
     except FileNotFoundError:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
