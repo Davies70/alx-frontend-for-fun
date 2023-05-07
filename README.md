e3e83cd44e4bcfee7b9321fc8a4e79b07f0afb15
Ajayi Davies <ajayidavies7@gmail.com>
2023-05-07 20:37:28 +0200

    tiny changes

--- README.md
+++ README.md
@@ -1,20 +1,4 @@
-# Welcome to Holberton
+Hello Holberton
 
-Nice school!
-
-- Webstack
-- AR/VR
-- ML
-- Low level
-- and more
-
-It's a super cool project.
-Like really. no?
-
-## Tasks
-
-* read requirements
-* quiz time
-* tasks
-
-In this project you will learn how to translate a Markdown to HTML.
+How are you?
+Fine and you?
--- markdown2html.py
+++ markdown2html.py
@@ -53,9 +53,17 @@ if __name__ == "__main__":
                         f.write("<li>" + lines[i][2:] + "</li>\n")
                 elif not lines[i] == "":
                     if i == 0:
-                        f.write("<p>\n" + lines[i])
+                        if len(lines) == 1:
+                            f.write("<p>\n" + lines[i] + "\n" + "</p>\n")
+                        elif lines[i + 1] == "":
+                            f.write("<p>\n" + lines[i] + "\n" + "</p>\n")
+                        else:
+                            f.write("<p>\n" + lines[i] + "\n" + "<br />\n")
                     elif i == len(lines) - 1:
-                        f.write("<p>\n" + lines[i] + "\n" + "</p>\n")
+                        if lines[i - 1] == "":
+                            f.write("<p>\n" + lines[i] + "\n" + "</p>\n")
+                        else:
+                            f.write(lines[i] + "\n" + "</p>\n")
                     elif lines[i - 1] == "":
                         f.write("<p>\n" + lines[i] + "\n")
                         if lines[i + 1] == "":
